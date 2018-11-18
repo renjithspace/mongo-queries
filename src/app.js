90b3cf3104e15ea55d7688ce4e1dfcc01be9bd60
renjithspace <renjithrajeevvk@gmail.com>
2018-11-18 16:47:32 +0530

    Emit queries to socket server when click

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 import Prism from 'prismjs'
+const io = require('socket.io-client')
+const config = require('../config')
+
+const socket = io(`http://${config.socket.host}:${config.socket.port}`)
+
+function emitQueries() {
+  const queryDOMs = document.querySelectorAll('.query')
+  
+  queryDOMs.forEach(queryDOM => {
+    queryDOM.addEventListener('click', () => {
+      const query = queryDOM.getAttribute('data-query')
+      socket.emit(query)
+    })
+  })
+}
+
+emitQueries()
