ddead0fd2d21cd60b694cb163263d6ace0de4f56
renjithspace <renjithrajeevvk@gmail.com>
2018-11-18 19:23:34 +0530

    Connection export as promise

--- queries/connect.js
+++ queries/connect.js
@@ -2,11 +2,14 @@ const mongodb = require('mongodb')
 const config = require('../config')
 
 const MongoClient = mongodb.MongoClient
-const db
 
-MongoClient.connect(config.mongodb_uri, { useNewUrlParser: true }, (error, client) => {
-  db = client.db('library')
-  client.close()
+const connect = new Promise((resolve, reject) => {
+  MongoClient.connect(config.mongodb_uri, { useNewUrlParser: true }, (error, client) => {
+    if (error) reject()
+
+    const db = client.db('library')
+    resolve(db)
+  })
 })
 
-module.exports = db
+module.exports = connect
