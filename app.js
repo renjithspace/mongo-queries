4b7f4ddea1d2ed72198d86ab3da53361d998042e
renjithspace <renjithrajeevvk@gmail.com>
2018-11-18 17:51:35 +0530

    Dynamically invoke queries based on socket payload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const socket = require('socket.io')
 const config = require('./config')
+const queries = require('./queries')
 
 const app = express()
 const server = app.listen(config.socket.port, config.socket.host)
@@ -8,5 +9,7 @@ const server = app.listen(config.socket.port, config.socket.host)
 const io = socket(server)
 
 io.on('connection', socket => {
-  socket.on('query', data => {})
+  socket.on('query', data => {
+    queries[data]
+  })
 })
