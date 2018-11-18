be2c48288831ba07841db3b1e63d6fc6589238d1
renjithspace <renjithrajeevvk@gmail.com>
2018-11-18 17:33:12 +0530

    Create socket serve and listen to query

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const socket = require('socket.io')
+const config = require('./config')
+
+const app = express()
+const server = app.listen(config.socket.port, config.socket.host)
+
+const io = socket(server)
+
+io.on('connection', socket => {
+  socket.on('query', data => {})
+})
